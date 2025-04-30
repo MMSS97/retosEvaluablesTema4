0ac614aeca5324eb889c4d4a24644d71b2517fa2
MMSS97 <michaelsorrosa@gmail.com>
2025-04-30 23:52:45 +0200

    dia1 terminado

---
+++ tema 1/reto1.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+let persona = {name:"Jose Antonio",
+            surname:"Arribas Pacios",
+            age: 14
+}
+
+fs.writeFile('personas.json', JSON.stringify(persona, null, 2), 'utf8', (error) =>{
+    fs.readFile('personas.json',('utf8'),(error, data) => {
+        let persona = JSON.parse(data);
+        console.log ('nombre:', persona.name)
+        console.log('salió según lo planeado')   
+    })
+}
+)
